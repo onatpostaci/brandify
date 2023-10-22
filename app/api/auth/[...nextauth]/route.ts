384d8316a1a1ffb2ad5914ed19e26cc84f1804c1
onatpostaci <onatpostman0gmail.com>
2023-10-22 23:42:10 +0300

    Route Fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,7 @@
 import NextAuth from "next-auth/next";
-import type { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import { authOptions } from "./options";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-export const OPTIONS: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-};
-
-const handler = NextAuth(OPTIONS);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
